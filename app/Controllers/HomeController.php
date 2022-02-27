71edcd6a129759e4ee96e8155fd2f6275a5b7b2d
= <lucasbritocientista@gmail.com>
2022-02-27 18:24:29 -0300

    13 - Configurando o Twig Template

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,6 +5,6 @@ namespace App\Controllers;
 class HomeController extends Controller {
 
   public function index($request, $response) {
-    return $response->write($this->container->hello);
+    return $this->container->view->render($response, 'test.twig');
   }
 }
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -14,6 +14,19 @@ $app = new Slim\App([
 
 $container = $app->getContainer();
 
+$container['view'] = function($container) {
+  $view = new Slim\Views\Twig(__DIR__ . "/../resources/views", [
+    'cache' => false,
+  ]);
+
+  $view->addExtension(new \Slim\Views\TwigExtension(
+    $container->router,
+    $container->request->getUri()
+  ));
+
+  return $view;
+};
+
 $container['hello'] = "Hello, World!";
 
 $container['HomeController'] = function($container) {
