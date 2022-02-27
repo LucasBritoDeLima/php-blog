f230ba750225a8c7f512501481c729577575d30f
= <lucasbritocientista@gmail.com>
2022-02-27 20:55:28 -0300

    16 - Estendendo Templates

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class AuthController extends Controller {
+
+  public function login($request, $response) {
+    return $this->container->view->render($response, 'login.twig');
+  }
+
+  public function register($request, $response) {
+    return $this->container->view->render($response, 'register.twig');
+  }
+}
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -33,4 +33,9 @@ $container['HomeController'] = function($container) {
   return new App\Controllers\HomeController($container);
 };
 
+$container['AuthController'] = function($container) {
+  return new App\Controllers\AuthController($container);
+};
+
+
 require __DIR__ . "/routes.php";
